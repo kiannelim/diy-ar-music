6c7159ec91bc4c49283e7d71b60adefa55abfa80
Glitch (hello-webpage) <none>
2020-07-01 08:17:37 +0000

    💐⛵️ Checkpoint ./background.js:9872069/6549 ./style.css:9872069/2441

--- background.js
+++ background.js
@@ -6,13 +6,13 @@ class Marker {
     this.timeout = MARKER_TIMEOUT_DEFAULT;
     this.timestamp = 0;
     this.present = false;
-    
-    this.center = {x:0, y:0};
+
+    this.center = { x: 0, y: 0 };
     this.corners = [];
-    
+
     this.id = ID;
   }
-  
+
   update(time) {
     this.present = time - this.timestamp > this.timeout ? false : this.present;
   }
@@ -27,15 +27,14 @@ function updateDetection() {
   dctx.lineWidth = 3;
 
   const timenow = Date.now();
-  markers.forEach((m) => {
-    
+  markers.forEach(m => {
     if (m.id < MARKER.length) {
       MARKER[m.id].present = true;
       MARKER[m.id].timestamp = timenow;
       MARKER[m.id].center = m.center;
       MARKER[m.id].corners = m.corners.map(c => c);
     }
-    
+
     const center = m.center;
     const corners = m.corners;
     const dctx = beholder.ctx;
@@ -58,45 +57,50 @@ function updateDetection() {
 
     dctx.strokeStyle = "#FF00AA";
     dctx.strokeRect(center.x - 1, center.y - 1, 2, 2);
-    
+
     dctx.font = "12px monospace";
     dctx.fillStyle = "#FF00AA";
     dctx.fillText(m.id, center.x + 5, center.y);
   });
-  
-  MARKER.forEach((m) => m.update(timenow));
-  
+
+  MARKER.forEach(m => m.update(timenow));
+
   requestAnimationFrame(updateDetection);
 }
 
-
-  
 window.onload = function() {
   // initialize detection stuff and hack in to detection loop
-  beholder.init('#detection-canvas');
-  
-  
-  // Detection param field changes
-  document.querySelector('#MIN_MARKER_DISTANCE').addEventListener('change', (e) => {
-    beholder.setParam('MIN_MARKER_DISTANCE', e.target.value)
-  });
-  document.querySelector('#MIN_MARKER_PERIMETER').addEventListener('change', (e) => {
-    beholder.setParam('MIN_MARKER_PERIMETER', e.target.value)
-  });
-  document.querySelector('#MAX_MARKER_PERIMETER').addEventListener('change', (e) => {
-    beholder.setParam('MAX_MARKER_PERIMETER', e.target.value)
-  });
-  document.querySelector('#SIZE_AFTER_PERSPECTIVE_REMOVAL').addEventListener('change', (e) => {
-    beholder.setParam('SIZE_AFTER_PERSPECTIVE_REMOVAL', e.target.value)
-  });
+  beholder.init("#detection-canvas");
 
-  const cameraSelect = document.querySelector('#CAMERA_INDEX');
-  beholder.getCameraFeeds().then((feeds) => {
-    cameraSelect.innerHTML = '';
+  // Detection param field changes
+  document
+    .querySelector("#MIN_MARKER_DISTANCE")
+    .addEventListener("change", e => {
+      beholder.setParam("MIN_MARKER_DISTANCE", e.target.value);
+    });
+  document
+    .querySelector("#MIN_MARKER_PERIMETER")
+    .addEventListener("change", e => {
+      beholder.setParam("MIN_MARKER_PERIMETER", e.target.value);
+    });
+  document
+    .querySelector("#MAX_MARKER_PERIMETER")
+    .addEventListener("change", e => {
+      beholder.setParam("MAX_MARKER_PERIMETER", e.target.value);
+    });
+  document
+    .querySelector("#SIZE_AFTER_PERSPECTIVE_REMOVAL")
+    .addEventListener("change", e => {
+      beholder.setParam("SIZE_AFTER_PERSPECTIVE_REMOVAL", e.target.value);
+    });
+
+  const cameraSelect = document.querySelector("#CAMERA_INDEX");
+  beholder.getCameraFeeds().then(feeds => {
+    cameraSelect.innerHTML = "";
 
     feeds.forEach((f, i) => {
-      if (f.kind === 'videoinput') {
-        const opt = document.createElement('option');
+      if (f.kind === "videoinput") {
+        const opt = document.createElement("option");
         opt.value = f.deviceId;
         opt.label = f.label ? f.label : i;
         if (i === 0) opt.selected = true;
@@ -104,20 +108,20 @@ window.onload = function() {
       }
     });
 
-    cameraSelect.addEventListener('change', (e) => {
+    cameraSelect.addEventListener("change", e => {
       console.log(e.target.value);
       beholder.setCamera(e.target.value);
     });
   });
-  
-  document.querySelector('#VIDEO_SIZE_INDEX').addEventListener('change', (e) => {
+
+  document.querySelector("#VIDEO_SIZE_INDEX").addEventListener("change", e => {
     console.log(e.target.value);
     beholder.setVideoSize(e.target.value);
   });
-  
-  for (let i=0; i<100; i++) {
+
+  for (let i = 0; i < 100; i++) {
     MARKER.push(new Marker(i));
   }
-  
+
   updateDetection();
-}
+};
