4e8390c6110e4fbcd4c48c9c71ab39c0116007f1
Glitch (hello-webpage) <none>
2020-07-02 02:24:17 +0000

    🚗🎖 Checkpoint ./background.js:9872069/2325

--- background.js
+++ background.js
@@ -11,12 +11,25 @@ class Marker {
 
     this.center = { x: 0, y: 0 };
     this.corners = [];
+    this.rotation = 0;
 
     this.id = ID;
   }
+  
+  updateMarker(m, timenow) {
+    this.present = true;
+    this.timestamp = timenow;
+    this.center = m.center;
+    this.corners = m.corners.map(c => c);
+  }
 
-  update(time) {
+  updatePresence(time) {
     this.present = time - this.timestamp > this.timeout ? false : this.present;
+  }
+  
+  updateRotation() {
+    this.rotation = vecAngleBetween(vecSub(this.corners[0], this.corners[1]), {x:0, y:0});
+    console.log(this.rotation);
   }
 }
 
@@ -65,7 +78,7 @@ function updateDetection() {
     dctx.fillText(m.id, center.x + 5, center.y);
   });
 
-  MARKER.forEach(m => m.update(timenow));
+  MARKER.forEach(m => m.updatePresence(timenow));
 
   requestAnimationFrame(updateDetection);
 }
@@ -136,3 +149,79 @@ window.onload = function() {
 
   updateDetection();
 };
+
+
+
+////////////////////
+//
+// Math methods
+//
+////////////////////
+
+function vecAdd(vec1, vec2) {
+  return {x:vec1.x + vec2.x, y:vec1.y + vec2.y};
+}
+
+// vector vec1 ---> vec2
+function vecSub(vec1, vec2) {
+  return {x:-vec1.x + vec2.x, y:-vec1.y + vec2.y};
+}
+
+function vecScale(vec, scale) {
+  return {x:vec.x*scale, y:vec.y*scale};
+}
+
+function vecDot(vec1, vec2) {
+  return vec1.x*vec2.x + vec1.y*vec2.y;
+}
+
+function vecMag(vec) {
+  return Math.pow(Math.pow(vec.x, 2) + Math.pow(vec.y, 2), 0.5);
+}
+
+function vecMag2(vec) {
+  return Math.pow(vec.x, 2) + Math.pow(vec.y, 2);
+}
+
+function vecUnit(vec) {
+  var m = vecMag(vec);
+  return {
+    x: vec.x/m,
+    y: vec.y/m,
+  };
+}
+
+function vecRot90(vec) {
+  return {x:vec.y, y:-vec.x}
+}
+
+function vecRot(vec, angle) {
+  var x = vec.x * Math.cos(angle) - vec.y * Math.sin(angle);
+  var y = vec.x * Math.sin(angle) + vec.y * Math.cos(angle);
+  return {x:x, y:y};
+}
+
+function vecAngleBetween(vec1, vec2) {
+  // return Math.atan2(vec1.y, vec1.x) - Math.atan2(vec2.y, vec2.x);
+  return Math.atan2(vec1.x*vec2.y-vec1.y*vec2.x, vec1.x*vec2.x+vec1.y*vec2.y);
+}
+
+function vecEMA(vec1, vec2, weight) {
+  return {
+    x: (vec1.x*(1-weight) + vec2.x*weight), 
+    y: (vec1.y*(1-weight) + vec2.y*weight) 
+  };
+}
+
+// Line closest point
+// p0 is point of interest, p1: start of line, p2: end of line
+function lineCP(p2, p0, p1) {
+  var p10 = {x: p0.x-p1.x, y: p0.y-p1.y};
+  var p12 = {x: p2.x-p1.x, y: p2.y-p1.y};
+  var t = vecDot(p12, p10) / vecDot(p12, p12);
+  var CPx = p1.x + t*p12.x;
+  var CPy = p1.y + t*p12.y;
+
+  return {x: CPx, y: CPy, t: t};
+}
+
