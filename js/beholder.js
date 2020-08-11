755eedddd61dea0b8fc80c58be52ef3ac9938ff4
Glitch (diy-ar-beholder) <none>
2020-08-11 08:39:43 +0000

    ⛺️🏠 Checkpoint ./js/beholder.js:9872069/692

--- js/beholder.js
+++ js/beholder.js
@@ -110,13 +110,29 @@ beholder.update = function update(){
     // Render video frame
     this.ctx.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
     imageData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
-
+    
+    imageData = contrastImage(imageData, -100);
+    this.ctx.putImageData(imageData, 0, 0);
+    
     var markers = this.detector.detect(imageData);
 
     this.updateCallbacks.forEach((cb) => {
       cb(markers, dt, this);
     });
   }
+}
+
+function contrastImage(imgData, contrast){  //input range [-100..100]
+    console.log(contrast);
+    var d = imgData.data;
+    contrast = (contrast/100) + 1;  //convert to decimal & shift range: [0..2]
+    var intercept = 128 * (1 - contrast);
+    for(var i=0;i<d.length;i+=4){   //r,g,b,a
+        d[i] = d[i]*contrast + intercept;
+        d[i+1] = d[i+1]*contrast + intercept;
+        d[i+2] = d[i+2]*contrast + intercept;
+    }
+    return imgData;
 }
 
 beholder.detect = function () {
